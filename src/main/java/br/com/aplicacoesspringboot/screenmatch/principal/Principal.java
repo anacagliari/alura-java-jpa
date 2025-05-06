e0b98e76c6a9d05ffbfa0df70d00ed8df96cf812
Ana Cagliari <111251330+anacagliari@users.noreply.github.com>
2025-05-06 14:26:02 -0300

    alura-java-jpa-aula01

--- src/main/java/br/com/aplicacoesspringboot/screenmatch/principal/Principal.java
+++ src/main/java/br/com/aplicacoesspringboot/screenmatch/principal/Principal.java
@@ -1,11 +1,15 @@
 package br.com.aplicacoesspringboot.screenmatch.principal;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import br.com.aplicacoesspringboot.screenmatch.model.DadosSerie;
 import br.com.aplicacoesspringboot.screenmatch.model.DadosTemporada;
+import br.com.aplicacoesspringboot.screenmatch.model.Serie;
 import br.com.aplicacoesspringboot.screenmatch.service.ConsumoApi;
 import br.com.aplicacoesspringboot.screenmatch.service.ConverteDado;
 
@@ -14,39 +18,55 @@ public class Principal {
     private ConsumoApi consumo = new ConsumoApi();
     private ConverteDado conversor = new ConverteDado();
 
-    private final String ENDERECO = "https://www.omdbapi.com/?t="; // constante (permite que valores fixos e imutáveis sejam armazenados e utilizados ao longo do código...)
-    private final String API_KEY = "&apikey=c6a5853b"; // constante (...muito úteis quando temos valores que não devem ser alterados durante a execução do programa. Nomenclatura: "snake_case")
+    private final String ENDERECO = "https://www.omdbapi.com/?t="; // constante (permite que valores fixos e imutáveis
+                                                                   // sejam armazenados e utilizados ao longo do
+                                                                   // código...)
+    private final String API_KEY = "&apikey=c6a5853b"; // constante (...muito úteis quando temos valores que não devem
+                                                       // ser alterados durante a execução do programa. Nomenclatura:
+                                                       // "snake_case")
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
 
     // Métodos alterados a partir da base do curso Alura Java - JPA
     public void exibeMenu() {
-        var menu = """
-                1 - Buscar séries
-                2 - Buscar episódios
-                
-                0 - Sair                                 
-                """;
+        var opcao = -1;
+        while (opcao != 0) {
 
-        System.out.println(menu);
-        var opcao = leitura.nextInt();
-        leitura.nextLine();
+            var menu = """
+                    1 - Buscar séries
+                    2 - Buscar episódios
+                    3 - Listar séries buscadas
 
-        switch (opcao) {
-            case 1:
-                buscarSerieWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida");
+                    0 - Sair
+                    """;
+
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSeriesBuscadas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
         }
     }
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        if (dados.titulo() != null) {
+            dadosSeries.add(dados);
+        }
         System.out.println(dados);
     }
 
@@ -58,7 +78,7 @@ public class Principal {
         return dados;
     }
 
-    private void buscarEpisodioPorSerie(){
+    private void buscarEpisodioPorSerie() {
         DadosSerie dadosSerie = getDadosSerie();
         List<DadosTemporada> temporadas = new ArrayList<>();
 
@@ -70,86 +90,127 @@ public class Principal {
         temporadas.forEach(System.out::println);
     }
 
-    /*
-    // Método original do curso Alura Java - Spring Boot
-    public void exibeMenu() {
-        System.out.println("Digite o nome da série: ");
-        var nomeSerie = leitura.nextLine();
-        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
+    private void listarSeriesBuscadas() {
+        List<Serie> series = new ArrayList<>();
+        series = dadosSeries.stream()
+                .map(dadosSerie -> {
+                    try {
+                        return new Serie(dadosSerie);
+                    } catch (UnsupportedEncodingException e) {
+                        System.err.println("Erro ao criar a série: " + dadosSerie.titulo());
+                        throw new RuntimeException("Falha ao processar os dados da série", e);
+                    }
+                })
+                .collect(Collectors.toList());
 
-        List<DadosTemporada> temporadas = new ArrayList<>();
-
-        for (int i = 1; i <= dados.totalTemporadas(); i++) {
-            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
-            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-            temporadas.add(dadosTemporada);
-        }
-        temporadas.forEach(System.out::println); // mesma coisa que: temporadas.forEach(t -> System.out.println(t));
-        
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo()))); 
-        //   lambda e stream
-        **LAMBDA (funções anônimas)        
-        São uma maneira de definir funções que são frequentemente usadas uma única vez, direto no local onde elas serão usadas. Principal vantagem é simplificar o código e mais legível.
-        t: temporadas - e: episodios. - substitui os for dentro de for, descrito abaixo. 
-        for (int i = 0; i < dados.totalTemporadas(); i++) {
-            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-            for (int j = 0; j < episodiosTemporada.size(); j++) {
-                System.out.println(episodiosTemporada.get(j).titulo());
-            }
-        } 
-        ** STREAM        
-        Forma de trabalhar com coleções de dados no Java. Elas permitem realizar operações de forma mais eficiente e concisa, utilizando uma abordagem funcional.
-        Uma stream é uma sequência de elementos que pode ser processada em paralelo ou em série. Ela pode ser criada a partir de uma coleção, um array, um arquivo, entre outros.
-        A partir daí, podemos realizar diversas operações nessa stream, como filtrar, mapear, ordenar, entre outras.
-        As operações intermediárias são aquelas que podem ser aplicadas em uma stream e retornam uma nova stream como resultado. Essas operações não são executadas imediatamente,
-        mas apenas quando uma operação final é chamada.
-        List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Rodrigo", "Nico");
-        nomes.stream()
-             .sorted()  //ordena os elementos
-             .limit(3)  //limita o número de elementos da stream
-             .filter(n -> n.startsWith("N"))  //permite filtrar os elementos da stream com base em uma condição. Por exemplo, podemos filtrar uma lista de números para retornar apenas os números pares.
-             .map(n -> n.toUpperCase())  //permite transformar cada elemento da stream em outro tipo de dado. Por exemplo, podemos transformar uma lista de strings em uma lista de seus respectivos tamanhos.
-             .forEach(System.out::println);  //permite executar uma ação em cada elemento da stream. Por exemplo, podemos imprimir cada elemento da lista. 
-        OUTRAS OPERAÇÕES
-        .collect //permite coletar os elementos da stream em uma coleção ou em outro tipo de dado. Por exemplo, podemos coletar os números pares em um conjunto. 
-        .distinct (que remove elementos duplicados)
-        .skip (que pula os primeiros elementos da stream)
-        .reduce (que combina os elementos da stream em um único resultado) 
-        
-        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
-                                    .flatMap(t -> t.episodios().stream())
-                                    .collect(Collectors.toList());
-        System.out.println("Top 5 episódios: ");
-        dadosEpisodios.stream()
-                      .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-                      .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                      .limit(5)
-                      .forEach(System.out::println);
-        
-        List<Episodio> episodios = temporadas.stream()
-                                             .flatMap(t -> t.episodios().stream()
-                                                                        .map(d -> new Episodio(t.numero(), d))
-                                            ).collect(Collectors.toList());
-
-        episodios.forEach(System.out::println);
-
-        System.out.println("A partir de que ano você deseja ver os episódios? ");
-        var ano = leitura.nextInt();
-        leitura.nextLine();
-
-        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
-
-        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        episodios.stream()
-                 .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
-                 .forEach(e -> System.out.println(
-                    "Temporada: " + e.getTemporada() +
-                    " | Episódio: " + e.getTitulo() +
-                    " | Data de lançamento: " + e.getDataLancamento().format(formatador)
-                 ));
+        series.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
     }
-    */
+
+    /*
+     * // Método original do curso Alura Java - Spring Boot
+     * public void exibeMenu() {
+     * System.out.println("Digite o nome da série: ");
+     * var nomeSerie = leitura.nextLine();
+     * var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +
+     * API_KEY);
+     * DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+     * System.out.println(dados);
+     * 
+     * List<DadosTemporada> temporadas = new ArrayList<>();
+     * 
+     * for (int i = 1; i <= dados.totalTemporadas(); i++) {
+     * json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&Season="
+     * + i + API_KEY);
+     * DadosTemporada dadosTemporada = conversor.obterDados(json,
+     * DadosTemporada.class);
+     * temporadas.add(dadosTemporada);
+     * }
+     * temporadas.forEach(System.out::println); // mesma coisa que:
+     * temporadas.forEach(t -> System.out.println(t));
+     * 
+     * temporadas.forEach(t -> t.episodios().forEach(e ->
+     * System.out.println(e.titulo())));
+     * // lambda e stream
+     ** LAMBDA (funções anônimas)
+     * São uma maneira de definir funções que são frequentemente usadas uma única
+     * vez, direto no local onde elas serão usadas. Principal vantagem é simplificar
+     * o código e mais legível.
+     * t: temporadas - e: episodios. - substitui os for dentro de for, descrito
+     * abaixo.
+     * for (int i = 0; i < dados.totalTemporadas(); i++) {
+     * List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+     * for (int j = 0; j < episodiosTemporada.size(); j++) {
+     * System.out.println(episodiosTemporada.get(j).titulo());
+     * }
+     * }
+     ** STREAM
+     * Forma de trabalhar com coleções de dados no Java. Elas permitem realizar
+     * operações de forma mais eficiente e concisa, utilizando uma abordagem
+     * funcional.
+     * Uma stream é uma sequência de elementos que pode ser processada em paralelo
+     * ou em série. Ela pode ser criada a partir de uma coleção, um array, um
+     * arquivo, entre outros.
+     * A partir daí, podemos realizar diversas operações nessa stream, como filtrar,
+     * mapear, ordenar, entre outras.
+     * As operações intermediárias são aquelas que podem ser aplicadas em uma stream
+     * e retornam uma nova stream como resultado. Essas operações não são executadas
+     * imediatamente,
+     * mas apenas quando uma operação final é chamada.
+     * List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Rodrigo",
+     * "Nico");
+     * nomes.stream()
+     * .sorted() //ordena os elementos
+     * .limit(3) //limita o número de elementos da stream
+     * .filter(n -> n.startsWith("N")) //permite filtrar os elementos da stream com
+     * base em uma condição. Por exemplo, podemos filtrar uma lista de números para
+     * retornar apenas os números pares.
+     * .map(n -> n.toUpperCase()) //permite transformar cada elemento da stream em
+     * outro tipo de dado. Por exemplo, podemos transformar uma lista de strings em
+     * uma lista de seus respectivos tamanhos.
+     * .forEach(System.out::println); //permite executar uma ação em cada elemento
+     * da stream. Por exemplo, podemos imprimir cada elemento da lista.
+     * OUTRAS OPERAÇÕES
+     * .collect //permite coletar os elementos da stream em uma coleção ou em outro
+     * tipo de dado. Por exemplo, podemos coletar os números pares em um conjunto.
+     * .distinct (que remove elementos duplicados)
+     * .skip (que pula os primeiros elementos da stream)
+     * .reduce (que combina os elementos da stream em um único resultado)
+     * 
+     * List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+     * .flatMap(t -> t.episodios().stream())
+     * .collect(Collectors.toList());
+     * System.out.println("Top 5 episódios: ");
+     * dadosEpisodios.stream()
+     * .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+     * .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+     * .limit(5)
+     * .forEach(System.out::println);
+     * 
+     * List<Episodio> episodios = temporadas.stream()
+     * .flatMap(t -> t.episodios().stream()
+     * .map(d -> new Episodio(t.numero(), d))
+     * ).collect(Collectors.toList());
+     * 
+     * episodios.forEach(System.out::println);
+     * 
+     * System.out.println("A partir de que ano você deseja ver os episódios? ");
+     * var ano = leitura.nextInt();
+     * leitura.nextLine();
+     * 
+     * LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+     * 
+     * DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+     * 
+     * episodios.stream()
+     * .filter(e -> e.getDataLancamento() != null &&
+     * e.getDataLancamento().isAfter(dataBusca))
+     * .forEach(e -> System.out.println(
+     * "Temporada: " + e.getTemporada() +
+     * " | Episódio: " + e.getTitulo() +
+     * " | Data de lançamento: " + e.getDataLancamento().format(formatador)
+     * ));
+     * }
+     */
 }
